ff6a76007eac25d5425dfb3c777b22bac8239d25
Hiromu OCHIAI <otiai10@gmail.com>
2017-08-14 16:58:02 +0900

    Add error code

--- encoder.go
+++ encoder.go
@@ -3,9 +3,27 @@ package goavcodec
 // #cgo pkg-config: libavformat libavcodec
 // #include <libavformat/avformat.h>
 // #include <libavcodec/avcodec.h>
+//
+// AVStream* get_stream(AVStream **streams, int pos)
+// {
+//   return streams[pos];
+// }
+//
+// AVFormatContext* out_context(char* name)
+// {
+//   AVFormatContext *out = avformat_alloc_context();
+//   snprintf(out->filename, sizeof(out->filename), "%s", name);
+//   return out;
+// }
+//
+// AVOutputFormat* out_format(AVFormatContext* out)
+// {
+//   return out->oformat;
+// }
 import "C"
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 )
@@ -14,6 +32,8 @@ import (
 type Encoder struct {
 	Context *C.struct_AVFormatContext
 	buf     []byte
+	src     *os.File
+	dst     *os.File
 }
 
 // NewEncoder ...
@@ -34,12 +54,55 @@ func NewEncoder(src *os.File) (*Encoder, error) {
 	if err := Error(ret); err != nil {
 		return nil, err
 	}
+
+	ret = C.avformat_find_stream_info(ctx, nil)
+	if err := Error(ret); err != nil {
+		return nil, err
+	}
+
 	return &Encoder{
 		Context: ctx,
+		src:     src,
 	}, nil
 }
 
 // Encode ...
 func (encoder *Encoder) Encode(out *os.File) error {
+
+	outpath, err := filepath.Abs(out.Name())
+	if err != nil {
+		return err
+	}
+	// C.av_dump_format(encoder.Context, C.int(0), C.CString(outpath), C.int(0))
+
+	index, codec := encoder.findIndex()
+
+	var packet C.struct_AVPacket
+	var finished C.int
+	frame := C.av_frame_alloc()
+	// var stream C.struct_AVStream
+
+	outctx := C.out_context(C.CString(outpath))
+	// format := C.out_format(outctx)
+	fmt.Printf("%T\n", outctx)
+	// fmt.Println("formatがnilか", format, out.Name())
+
+	for C.av_read_frame(encoder.Context, &packet) >= 0 {
+		if index == packet.stream_index {
+			C.avcodec_decode_video2(codec, frame, &finished, &packet)
+		}
+		C.av_packet_unref(&packet)
+	}
 	return nil
+}
+
+func (encoder *Encoder) findIndex() (C.int, *C.struct_AVCodecContext) {
+	var stream *C.struct_AVStream
+	for i := uint(0); i < uint(encoder.Context.nb_streams); i++ {
+		stream = C.get_stream(encoder.Context.streams, C.int(i))
+		if stream.codec.codec_type == C.AVMEDIA_TYPE_VIDEO {
+			return C.int(i), stream.codec
+		}
+	}
+	return 0, stream.codec
 }
--- errors.go
+++ errors.go
@@ -1,6 +1,7 @@
 package goavcodec
 
 import "C"
+import "fmt"
 
 // ftp://ftp.acer.com.tr/Archiv/gpl/AS9100/GPL_AS9100/MPlayer-0.90/libavcodec/avcodec.c
 func mktag(a, b, c, d int) int32 {
@@ -18,13 +19,36 @@ func (e *Err) Error() string {
 	return e.Message
 }
 
+// EOF represents "End of file" error, to specify end of read loop
+var EOF = &Err{-mktag('E', 'O', 'F', ' '), "End of file"}
+
 // "errors" represents all errors from ffmpeg
 // @see http://ffmpeg.org/doxygen/trunk/error_8h_source.html for more information
 var errors = []*Err{
-	&Err{
-		-mktag('I', 'N', 'D', 'A'),
-		"Invalid data found when processing input",
-	},
+	&Err{-mktag(0xF8, 'B', 'S', 'F'), "Bitstream filter not found"},
+	&Err{-mktag('B', 'U', 'G', '!'), "Internal bug, also see AVERROR_BUG2"},
+	&Err{-mktag('B', 'U', 'F', 'S'), "Buffer too small"},
+	&Err{-mktag(0xF8, 'D', 'E', 'C'), "Decoder not found"},
+	&Err{-mktag(0xF8, 'D', 'E', 'M'), "Demuxer not found"},
+	&Err{-mktag(0xF8, 'E', 'N', 'C'), "Encoder not found"},
+	EOF,
+	&Err{-mktag('E', 'X', 'I', 'T'), "Immediate exit was requested; the called function should not be restarted"},
+	&Err{-mktag('E', 'X', 'T', ' '), "Generic error in an external library"},
+	&Err{-mktag(0xF8, 'F', 'I', 'L'), "Filter not found"},
+	&Err{-mktag('I', 'N', 'D', 'A'), "Invalid data found when processing input"},
+	&Err{-mktag(0xF8, 'M', 'U', 'X'), "Muxer not found"},
+	&Err{-mktag(0xF8, 'O', 'P', 'T'), "Option not found"},
+	&Err{-mktag('P', 'A', 'W', 'E'), "Not yet implemented in FFmpeg, patches welcome"},
+	&Err{-mktag(0xF8, 'P', 'R', 'O'), "Protocol not found"},
+	&Err{-mktag(0xF8, 'S', 'T', 'R'), "Stream not found"},
+	&Err{-mktag('B', 'U', 'G', ' '), "AVError BUG"},
+	&Err{-mktag('U', 'N', 'K', 'N'), "Unknown error, typically from an external library"},
+	&Err{-mktag(0xF8, '4', '0', '0'), "AVERROR_HTTP_BAD_REQUEST"},
+	&Err{-mktag(0xF8, '4', '0', '1'), "AVERROR_HTTP_UNAUTHORIZED"},
+	&Err{-mktag(0xF8, '4', '0', '3'), "AVERROR_HTTP_FORBIDDEN"},
+	&Err{-mktag(0xF8, '4', '0', '4'), "AVERROR_HTTP_NOT_FOUND"},
+	&Err{-mktag(0xF8, '4', 'X', 'X'), "AVERROR_HTTP_OTHER_4XX"},
+	&Err{-mktag(0xF8, '5', 'X', 'X'), "AVERROR_HTTP_SERVER_ERROR"},
 }
 
 // Error provide specific error for code.
@@ -34,6 +58,9 @@ func Error(code C.int) error {
 		if err.Code == c {
 			return err
 		}
+	}
+	if c != 0 {
+		return &Err{Code: c, Message: fmt.Sprintf("Unhandled Error Code: %v", c)}
 	}
 	return nil
 }
